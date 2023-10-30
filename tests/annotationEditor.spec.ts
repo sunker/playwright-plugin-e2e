5e7c54ef425c8cbf01761b44a11e8195f2890dd1
Erik Sundell <erik.sundell87@gmail.com>
2023-10-30 10:24:29 +0100

    skip based on grafana version

--- tests/annotationEditor.spec.ts
+++ tests/annotationEditor.spec.ts
@@ -1,7 +1,13 @@
-import { test, expect } from '../src';
+const lte = require('semver/functions/lte');
+import { test } from '../src';
 import { RedshiftProvision } from './types';
 
-test('annotation editor', async ({ annotationEditPage, grafanaPage, selectors, readProvision }) => {
+test.skip;
+
+test('annotation editor', async ({ annotationEditPage, grafanaPage, selectors, readProvision, grafanaVersion }, {
+  skip,
+}) => {
+  skip(lte(grafanaVersion, '9.2.5'), 'Code Editor triggers one query per key down in Grafana 9.2.5 and below');
   const ds = await readProvision<RedshiftProvision>('datasources/aws-redshift.yaml').then((res) => res.datasources[0]);
   await annotationEditPage.setDataSource(ds.name);
   await grafanaPage.waitForFunction(() => (window as any).monaco);
