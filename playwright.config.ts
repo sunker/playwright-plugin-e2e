d89cf138e7a4d84830d7db443b7e58a3e4717858
Erik Sundell <erik.sundell87@gmail.com>
2023-11-07 11:18:52 +0100

    fix conf

--- playwright.config.ts
+++ playwright.config.ts
@@ -39,16 +39,6 @@ export default defineConfig<PluginOptions>({
 
   /* Configure projects for major browsers */
   projects: [
-    // Setup project
-    // {
-    //   name: 'createDataSource',
-    //   testMatch: /.*configEditor\.spec\.ts/,
-    //   dependencies: ['authenticate'],
-    //   use: {
-    //     ...devices['Desktop Chrome'],
-    //     storageState: 'playwright/.auth/user.json',
-    //   },
-    // },
     {
       name: 'authenticate',
       testMatch: [/.*auth\.setup\.ts/],
@@ -68,46 +58,6 @@ export default defineConfig<PluginOptions>({
         storageState: 'playwright/.auth/user.json',
       },
       dependencies: ['authenticate', 'setupDatasource'],
-      // dependencies: ['authenticate'],
     },
-
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    //   dependencies: ['setup'],
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    //   dependencies: ['setup'],
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
